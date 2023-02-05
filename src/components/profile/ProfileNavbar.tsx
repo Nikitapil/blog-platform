91ba3d72573289222d63a2096b50a714bd4b46f9
Nikitapil <nikitapiliavets@gmail.com>
2023-02-05 18:56:50 +0300

    hide personal link from others profile

--- src/components/profile/ProfileNavbar.tsx
+++ src/components/profile/ProfileNavbar.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 import styles from '../../assets/styles/profile.module.scss';
+import { useAppSelector } from '../../hooks/store/useAppSelector';
 
 export const ProfileNavbar = () => {
   const { id } = useParams();
+  const { user } = useAppSelector((state) => state.profile);
+  const { user: currentUser } = useAppSelector((state) => state.auth);
+
+  const isShowPersonalLink = useMemo(() => {
+    return user?.id === currentUser?.id;
+  }, [user, currentUser]);
+
   return (
     <nav className={styles['profile-navbar']}>
-      <NavLink
-        className={({ isActive }) => (isActive ? styles.active : undefined)}
-        to={`/profile/${id}/personal`}
-      >
-        Personal
-      </NavLink>
+      {isShowPersonalLink && (
+        <NavLink
+          className={({ isActive }) => (isActive ? styles.active : undefined)}
+          to={`/profile/${id}/personal`}
+        >
+          Personal
+        </NavLink>
+      )}
       <NavLink
         className={({ isActive }) => (isActive ? styles.active : undefined)}
         to={`/profile/${id}/user-posts`}

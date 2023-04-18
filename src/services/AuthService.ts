745f19ec90a613c4d44a93af6f0a7353cfe1f8fc
Nikitapil <nikitapiliavets@gmail.com>
2023-04-18 14:07:40 +0300

    refactor AuthService

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -6,23 +6,17 @@ export class AuthService {
   static async registration(
     formValues: AuthFormData
   ): Promise<AxiosResponse<TAuthResponse>> {
-    const response = await $api.post<TAuthResponse>(
-      '/auth/registration',
-      formValues
-    );
-    return response;
+    return $api.post<TAuthResponse>('/auth/registration', formValues);
   }
 
   static async login(
     formValues: AuthFormData
   ): Promise<AxiosResponse<TAuthResponse>> {
-    const response = await $api.post<TAuthResponse>('/auth/login', formValues);
-    return response;
+    return $api.post<TAuthResponse>('/auth/login', formValues);
   }
 
   static async checkAuth(): Promise<AxiosResponse<TAuthResponse>> {
-    const response = await $api.get<TAuthResponse>('/auth/refresh');
-    return response;
+    return $api.get<TAuthResponse>('/auth/refresh');
   }
 
   static async logout() {
